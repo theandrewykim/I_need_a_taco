8ea659f746af06a314d6d90f498a8a0026ae66f0
aykim <theandrewkimm@gmail.com>
2016-05-13 11:28:24 -0700

    Nearby Taco places show up on map

--- i_need_a_taco/app/assets/javascripts/index.js
+++ i_need_a_taco/app/assets/javascripts/index.js
@@ -2,62 +2,43 @@
  function tacoPlace(latitude, longitude, name, map) {
   this.latitude = latitude;
   this.longitude = longitude;
-  this.name = name;
+  this.restarauntName = name;
   this.map = map;
-  this.marker = new google.maps.Marker({position: new google.maps.LatLng(this.latitude, this.longitude)});
-  this.infowindow = new google.maps.InfoWindow({content: '<a href="recordings/'+this.id+'">'+this.title+'</a>'});
+  this.marker = new google.maps.Marker({position: new google.maps.LatLng(this.latitude, this.longitude), map: this.map});
   this.marker.setMap(this.map);
-  this.addListenerToMarker();
 }
 
 
-tacoPlace.prototype.addListenerToMarker = function() {
-  this.marker.addListener('click', function() {
-    this.infowindow.open(this.map, this.marker);
-  }.bind(this));
-};
-
-var userLatitude
-var userLongitude
 
 
-  function initialize() {
-    var mapProp = {
-      zoom:15,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-    var map=new google.maps.Map(document.getElementById("map"),mapProp);
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+var userLatitude;
+var userLongitude;
+var tacoMaps;
+var map;
+var marker;
 
-    if(navigator.geolocation) {
-      browserSupportFlag = true;
-      navigator.geolocation.getCurrentPosition(function(position) {
-        initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-        userLatitude = position.coords.latitude
-        userLongitude = position.coords.longitude
-        map.setCenter(initialLocation);
-    $("#button .button_to").on("submit", function(e) {
-    e.preventDefault()
-    $.ajax({
-      type: 'POST',
-      url: '/search',
-      data: {latitude: userLatitude, longitude: userLongitude},
-      dataType: 'json'
-    }).done(function(response) {
-      tacoEats = response.businesses
-      return new tacoPlace(tacoEats[0].location.coordinate.latitude, tacoEats[0].location.coordinate.longitude)
-      debugger
-    }).fail(function(error){
-      console.log("failed!")
-    })
 
-  });
-      }, function() {
-        handleNoGeolocation(browserSupportFlag);
-      });
-    }
+function initialize() {
+  var mapProp = {
+    zoom:15,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map"),mapProp);
+  var input = document.getElementById('pac-input');
+
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      userLatitude = position.coords.latitude
+      userLongitude = position.coords.longitude
+      map.setCenter(initialLocation);
+
+
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
     // Browser doesn't support Geolocation
     else {
       browserSupportFlag = false;
@@ -74,22 +55,22 @@ var userLongitude
   google.maps.event.addDomListener(window, 'load', initialize);
 
 
-$( document ).ready(function() {
-  // $("#button .button_to").on("submit", function(e) {
-  //   e.preventDefault()
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: '/search',
-  //     data: {latitude: userLatitude, longitude: userLongitude},
-  //     dataType: 'json'
-  //   }).done(function(response) {
-  //     tacoEats = response.businesses
-  //     new tacoPlace(tacoEats[0].location.coordinate.latitude, tacoEats[0].location.coordinate.longitude)
-  //   }).fail(function(error){
-  //     console.log("failed!")
-  //   })
+  $( document ).ready(function() {
+    $("#button .button_to").on("submit", function(e) {
+      e.preventDefault()
+      $.ajax({
+        type: 'POST',
+        url: '/search',
+        data: {latitude: userLatitude, longitude: userLongitude},
+        dataType: 'json'
+      }).done(function(response) {
+        tacoEats = response.businesses
+        tacoMaps = tacoEats.map(function(restaraunt) { return new tacoPlace(restaraunt.location.coordinate.latitude, restaraunt.location.coordinate.longitude, restaraunt.name, map) });
+      }).fail(function(error){
+        console.log("failed!")
+      })
 
-  // });
+    });
 
-});
+  });
 
